0fdf5a5c976ed9b87946b961e5cc745e4248b689
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2020-03-27 18:22:32 -0700

    Add files via upload

---
+++ Covid/src/handlers/get_expectedCases.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+from pymongo import MongoClient
+import requests
+import time
+
+def get_mongo_doc(db_name, collection_name):
+    db = client[db_name]
+    collection = db[collection_name]
+    pprint(collection.find_one())
+    print('\n')
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    get_mongo_doc('expectedCases', 'customer.expectedCases')
---
+++ Covid/src/handlers/get_mediaReportings.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+from pymongo import MongoClient
+import requests
+import time
+def get_mongo_doc(db_name, collection_name):
+    db = client[db_name]
+    collection = db[collection_name]
+    pprint(collection.find_one())
+    print('\n')
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    get_mongo_doc('mediaReportings', 'mediaReportings.cases')
